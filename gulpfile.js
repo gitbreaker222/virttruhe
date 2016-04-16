b41736822eed49c4a01f90e1cfdb8c33d63ef1b8
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-04-16 11:43:37 +0200

    adds task "compile_jade"

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
+var gulp = require('gulp');
+var packageJson = require('./package.json');
+var jade = require('gulp-jade');
+
 //convert jade files to html
+gulp.task('compile_jade', function() {
+  console.log(jade);
+  gulp.src('./src_app/*.jade')
+    .pipe(jade({
+      locals: {
+        title: packageJson.name
+      }
+    }))
+    .pipe(gulp.dest('./app/'))
+});
 
 //if jade files change, trigger convert task
