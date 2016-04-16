15c8c8302e9dbd297110543f032061c1dafab2a6
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-04-16 22:40:03 +0200

    adds init task and links run-sequence package

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
  **********************/
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
 var packageJson = require('./package.json');
 var del = require('del');
 var jade = require('gulp-jade');
@@ -41,7 +42,7 @@ gulp.task('delete:main.css', function() {
   return del.sync('app/style/main.css');
 });
 
-gulp.task('compile_copy_sass', ['delete:main.css'], function(){
+gulp.task('compile_sass', ['delete:main.css'], function(){
   gulp.src('src_app/style/main.sass')
     .pipe(sass())
     .pipe(gulp.dest('app/style'))
@@ -72,6 +73,22 @@ gulp.task('compile_coffee', function(){
  **********************/
 gulp.task('watch', function(){
   gulp.watch('./src_app/**/*.jade', ['compile_jade']);
-  gulp.watch('./src_app/**/*.sass', ['compile_copy_sass']);
+  gulp.watch('./src_app/**/*.sass', ['compile_sass']);
   gulp.watch('./src_app/modules/**/*.coffee', ['compile_coffee']);
+});
+
+/**********************
+ *
+ * DEFAULT / INIT
+ * * * * *
+ **********************/
+gulp.task('default', function(callback){
+  runSequence([
+    'compile_jade',
+    'compile_sass',
+    'compile_coffee'
+    ],
+    'watch',
+    callback
+  )
 });
