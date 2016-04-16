68dd0e48ed6036d215366d8e41e6ba16367316a7
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-04-16 16:29:59 +0200

    adds sass task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var packageJson = require('./package.json');
 var del = require('del');
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
 
 gulp.task('delete:index.html', function() {
   return del.sync('app/index.html');
@@ -19,6 +20,11 @@ gulp.task('compile_jade', ['delete:index.html'], function() {
     .pipe(gulp.dest('./app/'))
 });
 
+gulp.task('compile_copy_sass', function(){
+  gulp.src('src_app/style/main.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/style'))
+});
 
 gulp.task('watch', function(){
   gulp.watch('./src_app/**/*.jade', ['compile_jade']);
