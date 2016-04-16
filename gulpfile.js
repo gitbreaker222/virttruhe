dd7a1ae64fece3778c99626554b88c4dc7f24081
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-04-16 13:28:33 +0200

    adds del index.html task before jade task

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ var packageJson = require('./package.json');
 var del = require('del');
 var jade = require('gulp-jade');
 
+gulp.task('delete:index.html', function() {
+  return del.sync('app/index.html');
+});
+
 //convert jade files to html
-gulp.task('compile_jade', function() {
+gulp.task('compile_jade', ['delete:index.html'], function() {
   console.log(jade);
   gulp.src('./src_app/*.jade')
     .pipe(jade({
