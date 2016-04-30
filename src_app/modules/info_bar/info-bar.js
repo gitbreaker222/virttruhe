921b9727f0b3eb9e6a91b99f56784d993b355c07
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-04-30 23:55:11 +0200

    adds compiled infobar module

--- src_app/modules/info_bar/info-bar.js
+++ src_app/modules/info_bar/info-bar.js
@@ -1,5 +1,8 @@
-var InfoBar = {
-  controller: function() {},
+riot.tag2('info-bar', '<header> this is a header from riot.js <span class="hearts">hearts: {this.hearts}</span> </header> <form onsubmit="{updateLabel}"> <input type="text" name="inputHearts" placeholder="set hearts value"> <input type="submit" hidden> </form>', '', '', function(opts) {
 
-  view: function() {}
-};
+        this.hearts = this.opts.hearts;
+
+        this.updateLabel = function (){
+            this.hearts = this.inputHearts.value;
+        }
+});
