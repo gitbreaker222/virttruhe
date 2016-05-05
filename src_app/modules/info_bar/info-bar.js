7f4f70fe2eb7072d399db41e3d4805ebe6acca11
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-05-05 17:22:21 +0200

    resets input value after submit

--- src_app/modules/info_bar/info-bar.js
+++ src_app/modules/info_bar/info-bar.js
@@ -1,8 +1,9 @@
-riot.tag2('info-bar', '<header> this is a header from riot.js <span class="hearts">hearts: {this.hearts}</span> </header> <form onsubmit="{updateLabel}"> <input type="text" name="inputHearts" placeholder="set hearts value"> <input type="submit" hidden> </form>', '', '', function(opts) {
+riot.tag2('info-bar', '<header> this is a header from riot.js <input type="text" placeholder="hello" name="inputHearts" onchange="{updateLabel}"> <span class="hearts">hearts: {this.hearts}</span> </header>', '', '', function(opts) {
 
         this.hearts = this.opts.hearts;
 
         this.updateLabel = function (){
             this.hearts = this.inputHearts.value;
+            this.inputHearts.value = "";
         }
 });
