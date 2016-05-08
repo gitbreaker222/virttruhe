467c95cd0049814b4458934bc29aa3b2f70b47fd
Ruben La Biunda <ruben.labiunda@openmailbox.org>
2016-05-08 22:50:52 +0200

    remove compiled tag fragment from vcs

--- src_app/modules/info_bar/info-bar.js
+++
@@ -1,9 +0,0 @@
-riot.tag2('info-bar', '<header> this is a header from riot.js <input type="text" placeholder="hello" name="inputHearts" onchange="{updateLabel}"> <span class="hearts">hearts: {this.hearts}</span> </header>', '', '', function(opts) {
-
-        this.hearts = this.opts.hearts;
-
-        this.updateLabel = function (){
-            this.hearts = this.inputHearts.value;
-            this.inputHearts.value = "";
-        }
-});
