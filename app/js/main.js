b1ada06fb0eb713f27162edfd753cd0be92de319
Ruben L <ruben.labiunda@openmailbox.org>
2016-06-12 21:36:35 +0200

    updates built app

--- app/js/main.js
+++ app/js/main.js
@@ -340,143 +340,180 @@ app.getItems = function(){
   ];
 };
 
-riot.tag2('scanner', '<video id="cameraOutput" autoplay="true"> </video> <hr> <input type="file" accept="image"> <img src="./data/img/10000000 - visit virttruhe.tumblr.com.png" id="img">', '', '', function(opts) {
-    var checkDeviceSupport = app.services.mediaDevicesService.checkDeviceSupport;
-    var qr = new QCodeDecoder();
+riot.tag2('scanner', '<video id="cameraOutput" autoplay=""> </video> <hr> <input type="file" accept="image"> <img src="./data/img/10000000 - visit virttruhe.tumblr.com.png" id="img">', '', '', function(opts) {
+    var mediaSupportInfo = app.services.mediaDevicesService,
+        qr = new QCodeDecoder();
 
     var scope = this;
+    scope.hasWebcam = mediaSupportInfo.hasWebcam;
 
-    checkDeviceSupport(info);
+    var updateScope = function(){
+      scope.hasWebcam = mediaSupportInfo.hasWebcam;
+      scope.update();
+      playVideo(scope.cameraOutput);
+    };
 
-    function info() {
-      console.log(app.services.mediaDevicesService)
-    }
+    var videoError = function(e){
+      console.log(e);
+    };
+
+    var playVideo = function(video){
+
+      navigator.getUserMedia({ "video": true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      }, videoError)
+    };
+
+    mediaSupportInfo.checkDeviceSupport(updateScope);
 
 });
 /*
-  MEDIA-DEVICES-SERVICE.JS
+ GET-USER-MEDIA-SERVICE.JS
+ */
+function initGetUserMediaService(){
+  navigator.getUserMedia = navigator.getUserMedia || 
+    navigator.webkitGetUserMedia || 
+    navigator.mozGetUserMedia || //omit? 
+    navigator.msGetUserMedia || 
+    navigator.oGetUserMedia; //omit?
+}
+initGetUserMediaService();
+
+/*
+ END GET-USER-MEDIA-SERVICE.JS
+ */
+
+
+/*
+ MEDIA-DEVICES-SERVICE.JS
  */
 // based on http://stackoverflow.com/a/30047627/3313410
 
 app.services.mediaDevicesService = {};
-var scope = app.services.mediaDevicesService;
 
-if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-  // Firefox 38+ seems having support of enumerateDevices
-  navigator.enumerateDevices = function(callback) {
-    navigator.mediaDevices.enumerateDevices().then(callback);
-  };
-}
+function initMediaDevicesService (){
+  var scope = app.services.mediaDevicesService;
 
-scope.MediaDevices = [];
-scope.isHTTPs = location.protocol === 'https:';
-scope.canEnumerate = false;
-
-if (typeof MediaStreamTrack !== 'undefined' && 'getSources' in MediaStreamTrack) {
-  scope.canEnumerate = true;
-} else if (navigator.mediaDevices && !!navigator.mediaDevices.enumerateDevices) {
-  scope.canEnumerate = true;
-}
-
-scope.hasMicrophone = false;
-scope.hasSpeakers = false;
-scope.hasWebcam = false;
-
-scope.isMicrophoneAlreadyCaptured = false;
-scope.isWebcamAlreadyCaptured = false;
-
-scope.checkDeviceSupport = function (callback) {
-  if (!scope.canEnumerate) {
-    return;
+  if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+    // Firefox 38+ seems having support of enumerateDevices
+    navigator.enumerateDevices = function (callback) {
+      navigator.mediaDevices.enumerateDevices().then(callback);
+    };
   }
 
-  if (!navigator.enumerateDevices && window.MediaStreamTrack && window.MediaStreamTrack.getSources) {
-    navigator.enumerateDevices = window.MediaStreamTrack.getSources.bind(window.MediaStreamTrack);
+  scope.MediaDevices = [];
+  scope.isHTTPs = location.protocol === 'https:';
+  scope.canEnumerate = false;
+
+  if (typeof MediaStreamTrack !== 'undefined' && 'getSources' in MediaStreamTrack) {
+    scope.canEnumerate = true;
+  } else if (navigator.mediaDevices && !!navigator.mediaDevices.enumerateDevices) {
+    scope.canEnumerate = true;
   }
 
-  if (!navigator.enumerateDevices && navigator.enumerateDevices) {
-    navigator.enumerateDevices = navigator.enumerateDevices.bind(navigator);
-  }
+  scope.hasMicrophone = false;
+  scope.hasSpeakers = false;
+  scope.hasWebcam = false;
 
-  if (!navigator.enumerateDevices) {
-    if (callback) {
-      callback();
+  scope.isMicrophoneAlreadyCaptured = false;
+  scope.isWebcamAlreadyCaptured = false;
+
+  scope.checkDeviceSupport = function (callback) {
+    if (!scope.canEnumerate) {
+      return;
     }
-    return;
-  }
 
-  MediaDevices = [];
-  navigator.enumerateDevices(function(devices) {
-    devices.forEach(function(_device) {
-      var device = {};
-      for (var d in _device) {
-        device[d] = _device[d];
+    if (!navigator.enumerateDevices && window.MediaStreamTrack && window.MediaStreamTrack.getSources) {
+      navigator.enumerateDevices = window.MediaStreamTrack.getSources.bind(window.MediaStreamTrack);
+    }
+
+    if (!navigator.enumerateDevices && navigator.enumerateDevices) {
+      navigator.enumerateDevices = navigator.enumerateDevices.bind(navigator);
+    }
+
+    if (!navigator.enumerateDevices) {
+      if (callback) {
+        callback();
       }
+      return;
+    }
 
-      if (device.kind === 'audio') {
-        device.kind = 'audioinput';
-      }
-
-      if (device.kind === 'video') {
-        device.kind = 'videoinput';
-      }
-
-      var skip;
-      MediaDevices.forEach(function(d) {
-        if (d.id === device.id && d.kind === device.kind) {
-          skip = true;
+    MediaDevices = [];
+    navigator.enumerateDevices(function (devices) {
+      devices.forEach(function (_device) {
+        var device = {};
+        for (var d in _device) {
+          device[d] = _device[d];
         }
+
+        if (device.kind === 'audio') {
+          device.kind = 'audioinput';
+        }
+
+        if (device.kind === 'video') {
+          device.kind = 'videoinput';
+        }
+
+        var skip;
+        MediaDevices.forEach(function (d) {
+          if (d.id === device.id && d.kind === device.kind) {
+            skip = true;
+          }
+        });
+
+        if (skip) {
+          return;
+        }
+
+        if (!device.deviceId) {
+          device.deviceId = device.id;
+        }
+
+        if (!device.id) {
+          device.id = device.deviceId;
+        }
+
+        if (!device.label) {
+          device.label = 'Please invoke getUserMedia once.';
+          if (!scope.isHTTPs) {
+            device.label = 'HTTPs is required to get label of this ' + device.kind + ' device.';
+          }
+        } else {
+          if (device.kind === 'videoinput' && !scope.isWebcamAlreadyCaptured) {
+            scope.isWebcamAlreadyCaptured = true;
+          }
+
+          if (device.kind === 'audioinput' && !scope.isMicrophoneAlreadyCaptured) {
+            scope.isMicrophoneAlreadyCaptured = true;
+          }
+        }
+
+        if (device.kind === 'audioinput') {
+          scope.hasMicrophone = true;
+        }
+
+        if (device.kind === 'audiooutput') {
+          scope.hasSpeakers = true;
+        }
+
+        if (device.kind === 'videoinput') {
+          scope.hasWebcam = true;
+        }
+
+        // there is no 'videoouput' in the spec.
+
+        scope.MediaDevices.push(device);
       });
 
-      if (skip) {
-        return;
+      if (callback) {
+        callback();
       }
-
-      if (!device.deviceId) {
-        device.deviceId = device.id;
-      }
-
-      if (!device.id) {
-        device.id = device.deviceId;
-      }
-
-      if (!device.label) {
-        device.label = 'Please invoke getUserMedia once.';
-        if (!scope.isHTTPs) {
-          device.label = 'HTTPs is required to get label of this ' + device.kind + ' device.';
-        }
-      } else {
-        if (device.kind === 'videoinput' && !scope.isWebcamAlreadyCaptured) {
-          scope.isWebcamAlreadyCaptured = true;
-        }
-
-        if (device.kind === 'audioinput' && !scope.isMicrophoneAlreadyCaptured) {
-          scope.isMicrophoneAlreadyCaptured = true;
-        }
-      }
-
-      if (device.kind === 'audioinput') {
-        scope.hasMicrophone = true;
-      }
-
-      if (device.kind === 'audiooutput') {
-        scope.hasSpeakers = true;
-      }
-
-      if (device.kind === 'videoinput') {
-        scope.hasWebcam = true;
-      }
-
-      // there is no 'videoouput' in the spec.
-
-      scope.MediaDevices.push(device);
     });
+  }
+}
+initMediaDevicesService();
 
-    if (callback) {
-      callback();
-    }
-  });
-};
 /*
  MEDIA-DEVICES-SERVICE.JS END
  */
