8d856c2b80ce67ee203624938418b1c5045c4e42
Ruben L <ruben.labiunda@openmailbox.org>
2016-11-13 19:36:56 +0100

    adds demo model

---
+++ src_app/modules/_demo/demo-model.js
@@ -0,0 +1,33 @@
+/*
+ DEMO MODEL
+ */
+app.models.Demo = function () {
+  // Make instances observable
+  riot.observable(this);
+  
+  // variables
+  var greetingText = 'Servus';
+  
+  // private methods
+  var greet = function (text) {
+    //validation
+    text = text || greetingText;
+    if (typeof(text) !== 'string') {
+      window.console.error('Expected string, instead got: '+typeof(text));
+    }
+    //console.log(text)
+  };
+  
+  // public methods
+  this.test = function () {
+    window.console.log('this is a test. Have this app object:', app);
+  };
+  
+  var init = function () {
+    greet();
+  };
+  app.on('initInstances', init);
+};
+/*
+ DEMO MODEL END
+ */
